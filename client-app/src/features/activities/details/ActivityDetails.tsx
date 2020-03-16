73452338dbf3dae1c9a73896996ec0fa8e16bef3
Tim Remmert <tremmert93@gmail.com>
2020-03-16 09:55:07 -0400

    Adding page styling

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,17 +1,23 @@
 import React, { useContext, useEffect } from 'react'
-import { Card, Image, Button } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import ActivityStore from '../../../app/stores/activityStore'
 import { observer } from 'mobx-react-lite'
-import { RouteComponentProps, Link } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router-dom'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
+import ActivityDetailedSidebar from './ActivityDetailedSidebar'
+import ActivityDetailsHeader from './ActivityDetailsHeader'
+import ActivityDetailedInfo from './ActivityDetailedInfo'
+import ActivityDetailedChat from './ActivityDetailedChat'
 
 interface DetailParams {
   id: string
 }
 
-const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match }) => {
   const activityStore = useContext(ActivityStore)
-  const {activity, loadActivity, loadingInitial} = activityStore;
+  const { activity, loadActivity, loadingInitial } = activityStore;
+
+  console.log(activity)
 
   useEffect(() => {
     loadActivity(match.params.id)
@@ -19,26 +25,18 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, h
 
   if (loadingInitial || !activity) return <LoadingComponent content='loading activities...' />
 
-    return (
-        <Card fluid>
-        <Image src={`/assets/${activity!.category}.jpg`} wrapped ui={false} />
-        <Card.Content>
-    <Card.Header>{activity!.title}</Card.Header>
-          <Card.Meta>
-            <span>{activity!.date}</span>
-          </Card.Meta>
-          <Card.Description>
-            {activity!.description}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <Button.Group widths={2}>
-                <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit'/>
-                <Button onClick={() => history.push('/activities')} basic color='grey' content='Cancel' />
-            </Button.Group>
-        </Card.Content>
-      </Card>
-    )
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailsHeader activity={activity}/>
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar />
+      </Grid.Column>
+    </Grid>
+  )
 }
 
 export default observer(ActivityDetails)
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState, FormEvent, useContext, useEffect} from 'react'
-import { Segment, Form, Button } from 'semantic-ui-react'
+import { Segment, Form, Button, Grid } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import {v4 as uuid} from 'uuid'
 import ActivityStore from '../../../app/stores/activityStore'
@@ -62,7 +62,9 @@ const ActivityForm:React.FC<RouteComponentProps<DetailParams>> = ({ match, histo
   }
 
     return (
-      <Segment clearing>
+      <Grid>
+        <Grid.Column width={10}>
+        <Segment clearing>
           <Form onSubmit={handleSubmit}>
             <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title}/>
             <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description}/>
@@ -79,6 +81,9 @@ const ActivityForm:React.FC<RouteComponentProps<DetailParams>> = ({ match, histo
             <Button onClick={() => history.push('/activities')} floated='right' type='button' content='Cancel'/>
           </Form>
       </Segment>
+        </Grid.Column>
+      </Grid>
+      
     )
 }
 
